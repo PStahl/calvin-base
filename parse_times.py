ce7ba94a0c76289ef3fbacbbff2cc5b76ba1097e
pstahl <philip.stahl91@gmail.com>
2016-04-07 17:03:12 +0200

    update

--- parse_times.py
+++ parse_times.py
@@ -1,16 +1,17 @@
 import datetime
 
-with open("dave", "r") as f:
+with open("failure_times", "r") as f:
     lines = f.readlines()
     total = 0
     count = 0
-    for line in lines:
-        t1 = line.split(",")[0][:12]
-        print t1[:12]
-        t1 = datetime.datetime.strptime(t1, "%M:%S.%f")
-        t2 = line.split(",")[1][:12]
-        t2 = datetime.datetime.strptime(t2, "%M:%S.%f")
+    for i in range(len(lines) - 1):
+        t1 = lines[i].split()[1][:12]
+        t1 = datetime.datetime.strptime(t1, "%H:%M:%S.%f")
+        print t1
+        t2 = lines[i + 1].split()[1][:12]
+        t2 = datetime.datetime.strptime(t2, "%H:%M:%S.%f")
+        print t2
         diff = (t2 - t1).microseconds
         total += diff
         count += 1
-    print float(total) / count
+    print float(total) / (count * 1000)  # to ms
