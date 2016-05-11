b554d3fcc630ab0ac4cc369c147cd91a8b020360
pstahl <philip.stahl91@gmail.com>
2016-05-11 12:35:27 +0200

    update print scripts

--- test_scripts/print_total_times.sh
+++ test_scripts/print_total_times.sh
@@ -1,2 +1,11 @@
 ./clean_rep_times.sh
+echo ""
+./print_encode_times.sh
+echo ""
+./print_transmit_times.sh
+echo ""
+./print_decode_times.sh
+echo ""
+./print_create_actor_times.sh
+echo ""
 find rep_times/* -type f -print0 | sort -z -t/ -k2 -n | xargs -0 sed -n '5~5p'
