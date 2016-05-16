ece7a9a1b4bd690a96110ca7c01cf0e7005b2976
pstahl <philip.stahl91@gmail.com>
2016-05-16 15:06:55 +0200

    update experiment tests

--- test_scripts/run_and_kill.sh
+++ test_scripts/run_and_kill.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 #rand=`python -c "import random; print random.randint(30, 90)"`
-rands=($(python -c "import numpy as np;
-mu=18.9; sigma=1.0;
-s = np.random.normal(mu, sigma, 1000)
-for n in s:
-        print n
-"))
+#rands=($(python -c "import numpy as np;
+#mu=18.9; sigma=1.0;
+#s = np.random.normal(mu, sigma, 1000)
+#for n in s:
+#        print n
+#"))
+#i=0
 i=0
 
 n=$1
@@ -17,21 +19,26 @@ controlport=$(($port+1))
 host=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
 
 extra=0
+#echo $n
+#echo $host
+#echo $port
+#echo $controlport
 
 while true; do
-    rand=${rands[i]}
-    echo $rand
+    #rand=${rands[i]}
+    rand=2
+    #echo $rand
     rand=`awk "BEGIN {print ($rand - $extra)}"`
     extra=0
     i=$((i+1))
-    echo "random: $rand"
+    #echo "random: $rand"
 
     #[ -f calvin.conf.bak ] && mv calvin.conf.bak calvin.conf
     #echo "Starting runtime on port $port - $controlport"
     #csruntime --host `hostname -A | awk '{print $1}'` --port $port --controlport $controlport --keep-alive -l ERROR -l 'calvin.runtime.north.lost_node_handler:DEBUG' -l 'calvin.runtime.north.replicator:DEBUG' -l 'calvin.runtime.north.portmanager:DEBUG' &
 	./start.sh $port $controlport &
     PID=$!
-    echo "PID: $PID"
+    #echo "PID: $PID"
 
     sleep 0.5
     #echo `ps aux | grep -E "(csruntime|py.test)"`
@@ -39,11 +46,13 @@ while true; do
     #echo "cscontrol http://gru.nefario:5002 nodes add calvinip://$host:$port"
     nodes_add="null"
     regex="null|Read timed out"
-    while [[ "$nodes_add" =~ $regex ]];
+    tries=0
+    while [[ "$nodes_add" =~ $regex ]] && [[ $tries != 4 ]];
     do
         #echo "adding node"
         #cscontrol http://gru.nefario:5002 nodes list
         nodes_add=`cscontrol http://gru.nefario:5002 nodes add calvinip://$host:$port`
+        tries=$(($tries+1))
         echo $nodes_add
     done
     #nodes_add=`cscontrol http://gru.nefario:5002 nodes add calvinip://$host:$port`
@@ -54,21 +63,10 @@ while true; do
     #echo "BEFORE: $before"
     #pstree -p
 
-    printf "\n\n"
-    date +"%Y-%m-%d %H:%M:%S.%N"
-    printf "\n\n"
-
-    nodes=`cscontrol http://gru.nefario:5002 nodes list`
-    n_nodes=$(grep -o " " <<< "$nodes" | wc -l)
-    while [ $n_nodes -lt 3 ]
-    do
-        echo "not enough nodes, sleeping..."
-        echo $n_nodes
-        nodes=`cscontrol http://gru.nefario:5002 nodes list`
-        n_nodes=$(grep -o " " <<< "$nodes" | wc -l)
-        sleep 0.2
-        extra=`awk "BEGIN {print ($extra + 0.2)}"`
-    done
-    pkill -9 -P $PID
+    #printf "\n\n"
+    #date +"%Y-%m-%d %H:%M:%S.%N"
+    #printf "\n\n"
+    #pkill -9 -P $PID
+    pkill -P $PID
     sleep 0.2
 done
